2f331ecd56812a3915545899b736728a1646a343
ChristopherSchoening <schoeningchristopher@gmail.com>
2024-12-23 15:05:25 +0100

    add debug log

--- routes/licenses/index.tsx
+++ routes/licenses/index.tsx
@@ -8,6 +8,9 @@ export const handler: Handlers<LicenseRouteProps> = {
   async GET(_, ctx) {
     const url = `${Deno.env.get("API_URL")}/licenses`
     const response = await fetch(url)
+    if (!response.ok) {
+      return ctx.render({ licenseInfo: `error ${response}, ${url}` })
+    }
     const licenseInfo = await response.text()
     console.log("🚀 ~ GET ~ licenseInfo:", licenseInfo)
 
