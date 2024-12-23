9d5a09d46ece28feaed98703c965fe92aaa1adce
ChristopherSchoening <schoeningchristopher@gmail.com>
2024-12-23 15:20:00 +0100

    add debug log

--- routes/api/licenses/index.ts
+++ routes/api/licenses/index.ts
@@ -3,8 +3,10 @@ import licenseInfo from "./license-info.json" with { type: "json" }
 import type { LicenseRouteProps } from "../../licenses/index.tsx"
 
 export const handler: Handlers<{ licenseText: LicenseRouteProps }> = {
-  GET: async () =>
-    Response.json(licenseInfo, {
+  GET: async (req) => {
+    console.log("API request received:", req.url)
+    return Response.json(licenseInfo, {
       headers: { "Content-Type": "application/json" },
-    }),
+    })
+  },
 }
