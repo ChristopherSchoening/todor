06ba893d3cf1cff05d269b45c6f8c1b8823686ec
ChristopherSchoening <schoeningchristopher@gmail.com>
2024-12-23 16:46:44 +0100

    fix formatting

--- routes/licenses/_test.ts
+++ routes/licenses/_test.ts
@@ -4,19 +4,19 @@ import config from "../../fresh.config.ts"
 import manifest from "../../fresh.gen.ts"
 
 const CONN_INFO: ServeHandlerInfo = {
-	remoteAddr: { hostname: "127.0.0.1", port: 53496, transport: "tcp" },
-	completed: Promise.resolve(),
+  remoteAddr: { hostname: "127.0.0.1", port: 53496, transport: "tcp" },
+  completed: Promise.resolve(),
 }
 
 Deno.test("get licenses", async () => {
-	const handler = await createHandler(manifest, config)
+  const handler = await createHandler(manifest, config)
 
-	const resp = await handler(
-		new Request("http://127.0.0.1/api/licenses"),
-		CONN_INFO,
-	)
-	const responseContent = await resp.json()
+  const resp = await handler(
+    new Request("http://127.0.0.1/api/licenses"),
+    CONN_INFO,
+  )
+  const responseContent = await resp.json()
 
-	expect(resp.status).toEqual(200)
-	expect(responseContent.length).toBeGreaterThan(0)
+  expect(resp.status).toEqual(200)
+  expect(responseContent.length).toBeGreaterThan(0)
 })
